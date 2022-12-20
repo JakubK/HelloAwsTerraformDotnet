8bf24b11a0d8e2c983b11b11dc78469cff7d68b7
Jakub Kuniszewski <you@example.com>
2022-12-20 23:44:38 +0100

    Remove resources already covered in Scenario

--- main.tf
+++ main.tf
@@ -7,10 +7,3 @@ resource "random_pet" "frontend_bucket_name" {
   prefix = "learn-terraform-functions"
   length = 4
 }
-
-data "archive_file" "lambda_hello_world" {
-  type = "zip"
-
-  source_dir  = "${path.module}/SimpleAPI/bin/Debug/net6.0"
-  output_path = "${path.module}/${var.lambda_archive}"
-}
