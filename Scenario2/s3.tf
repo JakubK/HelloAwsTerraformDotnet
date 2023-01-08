684a6517f44355a14c1a13e29f7e323f02785319
Jakub Kuniszewski <you@example.com>
2023-01-08 16:43:52 +0100

    Setup file upload trigger event

--- Scenario2/s3.tf
+++ Scenario2/s3.tf
@@ -1,11 +1,16 @@
 resource "aws_s3_bucket" "zip_bucket" {
+  bucket = "zip-bucket"
+  acl = "private"
 }
 
 resource "aws_s3_bucket" "unzip_bucket" {
+  bucket = "unzip-bucket"
+  acl = "private"
 }
 
 resource "aws_s3_bucket" "lambda_bucket" {
-  
+  bucket = "lambda-source"
+  acl = "private"
 }
 
 resource "aws_s3_object" "lambda_source" {
@@ -15,4 +20,13 @@ resource "aws_s3_object" "lambda_source" {
   source = data.archive_file.lambda_zip.output_path
 
   etag = filemd5(data.archive_file.lambda_zip.output_path)
+}
+
+resource "aws_s3_bucket_notification" "zip_upload_notification" {
+  bucket = aws_s3_bucket.zip_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.unzip_lambda.arn
+    events = ["s3:ObjectCreated:*"]
+  }
 }
